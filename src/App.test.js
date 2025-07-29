52fec9a495bb6235bab98987729f3a03e03f56d3
Sherwin <fivefullsummers@gmail.com>
2025-07-29 15:27:49 +0200

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hi There broskie!/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+
+
